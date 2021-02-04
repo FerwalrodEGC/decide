27230e291ce5722239e45d0fac2fb28dfe6bb885
enderfews <ferwalrod@alum.us.es>
2021-02-04 08:50:48 +0100

    cambio en el runtime

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -185,3 +184,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
